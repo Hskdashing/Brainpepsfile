afe4527eda0ee01dde0b594876122ac3f6bcab8a
Hsk <149724465+Hskdashing@users.noreply.github.com>
2024-01-10 09:06:12 +0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,5 +1,5 @@
 # Credits: @hskdashing
-# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# FROM File-Sharing-Man <https://github.com/Hskdashing/File-Sharinghsk/>
 # t.me/hskdashing & t.me/hskdashing
 
 from pyrogram.types import InlineKeyboardButton
